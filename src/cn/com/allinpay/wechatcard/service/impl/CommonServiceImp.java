43b9644285c347af34b1058788ef31bd2dff87d8
zhangxiaodang <zhangxiaodang@126.com>
2015-08-13 23:26:46 +0800

    update

--- src/cn/com/allinpay/wechatcard/service/impl/CommonServiceImp.java
+++ src/cn/com/allinpay/wechatcard/service/impl/CommonServiceImp.java
@@ -3,18 +3,28 @@ package cn.com.allinpay.wechatcard.service.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.com.allinpay.frame.service.BaseService;
 import cn.com.allinpay.wechatcard.dao.CommonDao;
 import cn.com.allinpay.wechatcard.service.ICommonService;
+import cn.com.weixin.api.result.OAuthCodeResult;
 import cn.com.weixin.api.util.WeixinMsgCommonUtil;
+
+/**
+ * 共通Service实现类.
+ */
 
 @Service
 public class CommonServiceImp extends BaseService implements ICommonService {
 
-	/** 注册的dao **/
+	/** logger **/
+	private static final Logger logger = Logger
+			.getLogger(CommonServiceImp.class);
+
+	/** 共通dao **/
 	@Autowired
 	private CommonDao dao;
 
@@ -24,16 +34,39 @@ public class CommonServiceImp extends BaseService implements ICommonService {
 		// OpenID
 		String strOpenId = "";
 
-		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put(BEAN, urlFlag);
-
+		// 根据商户标识取得商户微信服务号的appid和appsecrt
 		Map<String, String> appInfo = this.dao.getAppInfo(urlFlag);
 
 		// 根据Code取得openID
-		strOpenId = WeixinMsgCommonUtil.getOpenIDByOAuth(code,
-				appInfo.get("appid"), appInfo.get("appsecret")).getOpenid();
+		OAuthCodeResult result = WeixinMsgCommonUtil.getOpenIDByOAuth(code,
+				appInfo.get("appid"), appInfo.get("appsecret"));
+
+		// 成功时
+		if (result != null && !result.getErrmsg().equals("")) {
+			strOpenId = result.getOpenid();
+		} else {
+			// 失败时
+			logger.info("取得用户openid失败,errcode:" + result.getErrcode()
+					+ ",errmsg:" + result.getErrmsg());
+		}
 
 		// 返回
 		return strOpenId;
+	}
+
+	@Override
+	public boolean isRegister(String urlFlag, String openid) {
+
+		Map<String, String> paramMap = new HashMap<String, String>();
+		// urlflag
+		paramMap.put("urlflag", urlFlag);
+		// openid
+		paramMap.put("openid", openid);
+
+		// 查询是否注册
+		int result = this.dao.getRegister(paramMap);
+
+		// 返回
+		return result == 0 ? false : true;
 	}
 }
