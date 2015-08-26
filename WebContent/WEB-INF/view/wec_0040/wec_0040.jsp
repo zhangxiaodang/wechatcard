0f332841598220a3d3f0c05e4a95df99b17ec523
zhangxiaodang <zhangxiaodang@126.com>
2015-08-26 23:42:06 +0800

    a

--- WebContent/WEB-INF/view/wec_0040/wec_0040.jsp
+++ WebContent/WEB-INF/view/wec_0040/wec_0040.jsp
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>我的优惠券</title>
+    <title>优惠券列表</title>
     <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=no" />
     <meta name="apple-mobile-web-app-capable" content="yes"/>
     <meta name="apple-touch-fullscreen" content="yes"/>
--- src/cn/com/allinpay/wechatcard/controller/WEC0040Controller.java
+++ src/cn/com/allinpay/wechatcard/controller/WEC0040Controller.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import cn.com.allinpay.frame.controller.BaseController;
 import cn.com.allinpay.frame.util.WebConstantUrlValue;
@@ -38,10 +39,56 @@ public class WEC0040Controller extends BaseController {
 	 * 注册页面URL.
 	 */
 	@RequestMapping(value = WebConstantUrlValue.WEC0040_INDEX, method = RequestMethod.GET)
-	public String getPageIndex() {
+	public ModelAndView getPageIndex() {
+		// 商户标识
+		String strUrlFlag = "";
+		// 网页Code
+		String strCode = "";
+		// openid
+		String strOpenID = "";
+		// 返回值
+		ModelAndView mv = new ModelAndView();
 
+		try {
+			strUrlFlag = super.request.getParameter(KEY_URL_FLAG);
+			strCode = super.request.getParameter(KEY_CODE);
+
+			super.logger.info("zhangxd==获取网页code：" + strCode);
+			super.logger.info("zhangxd==获取网页urlflag：" + strUrlFlag);
+
+			// 放到session中
+			super.session.setAttribute(SESSION_KEY_URLFLAG, strUrlFlag);
+
+			// 从session中取得openid
+			strOpenID = (String) super.session.getAttribute(SESSION_KEY_OPENID);
+			// 从其它页面跳转过来时
+			if (strOpenID == null || strOpenID.isEmpty()) {
+				// 取得OpenID
+				strOpenID = this.commonService.getOpenID(strUrlFlag, strCode);
+			}
+			logger.info("zhangxd取得的openid为:" + strOpenID);
+			
+			// todo-zhangxd
+			strOpenID = "asdfasdfoo";
+
+			// 未获取openid时
+			if (strOpenID == null || strOpenID.isEmpty()) {
+				logger.info("未获取到openid!");
+				mv.addObject("errmsg", "未获取到openid!");
+				mv.setViewName(WebConstantUrlValue.WEC_ERROR);
+			} else {
+				// 放到Session中
+				super.session.setAttribute(SESSION_KEY_OPENID, strOpenID);
+				logger.info("用户openid:" + strOpenID);
+				mv.setViewName(WEC0040_VIEW);
+			}
+		} catch (Exception e) {
+			logger.info("异常：\n" + e.getMessage());
+			mv.addObject("errmsg", "打开页面时异常");
+			mv.setViewName(WebConstantUrlValue.WEC_ERROR);
+		}
 		// 返回
-		return WEC0040_VIEW;
+		return mv;
 	}
 
 	/**
@@ -62,7 +109,7 @@ public class WEC0040Controller extends BaseController {
 					.getAttribute(SESSION_KEY_OPENID);
 			memberView.setOpenid(strOpenID);
 			memberView.setUrlflag(strUrlFlag);
-			// 调用注册的service
+			// 调用注册的service 
 			resultModel = wec0040Service.get_coupon(memberView);
 		} catch (Exception e) {
 			logger.info("========================Exception my_coupon Start==========================");
