982ff9ed68fe92397258f434dd5d6c201262febe
margin <zhangxiaodang@126.com>
2015-09-05 22:35:09 +0800

    解决冲突

--- src/cn/com/allinpay/frame/util/WebConstantUrlValue.java
+++ src/cn/com/allinpay/frame/util/WebConstantUrlValue.java
@@ -29,10 +29,10 @@ public class WebConstantUrlValue {
 
 	/** 绑定旧卡页面的URL. */
 	public static final String WEC0022_INDEX = "/wec0022";
-	
+
 	/** 重新绑定页面的URL. */
 	public static final String WEC0023_INDEX = "/wec0023";
-	
+
 	/** 重新绑定的URL. */
 	public static final String CHANGE_OPENID = "/wec0023/change_openid";
 
@@ -50,6 +50,15 @@ public class WebConstantUrlValue {
 
 	/** 获取会员卡信息的URL. */
 	public static final String WEC0031_GET_INFO = "/get_card_info";
+	
+	/** 消费记录URL. */
+	public static final String WEC0033_INDEX = "/wec0033";
+
+	/** 消费记录URL. */
+	public static final String WEC0033_GET_CONSUME = "/wec0033/get_consume";
+	
+	/** 我要充值页面的URL. */
+	public static final String WEC0032_INDEX = "/wec0032";
 
 	/** 优惠券页面的URL. */
 	public static final String WEC0034_INDEX = "/wec0034";
@@ -62,26 +71,26 @@ public class WebConstantUrlValue {
 
 	/** 修改卡密码URL **/
 	public static final String WEC0035_CHANGE_PASS = "/change_pass";
-	
+
 	/** 我的优惠券URL. */
 	public static final String WEC0040_INDEX = "/wec0040";
 	/** 我的优惠券URL. */
 	public static final String WEC0040_MY_COUPON = "/wec0040/my_coupon";
-	
+
 	/** 优惠券领取的URL **/
 	public static final String WEC0041_INDEX = "/wec0041";
 	/** 优惠券领取URL **/
 	public static final String WEC0041_COLLECT_COUPON = "/wec0041/collect_coupon";
-	
+
 	/** 优惠券领取完毕的URL **/
 	public static final String WEC0042_INDEX = "/wec0042";
-	
+
 	/** 商家优惠券URL. */
 	public static final String WEC0043_INDEX = "/wec0043";
-	
+
 	/** 商家优惠券列表URL. */
 	public static final String WEC0043_MERCHANTCOUPON_LIST = "/wec0043/merchantcoupon_list";
-	
+
 	/** 我要付款的URL **/
 	public static final String WEC0050_INDEX = "/wec0050";
 
